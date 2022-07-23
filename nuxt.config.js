ded47c9eca88cbddb56f589e017cc595dab3691c
Nikolas Castro <nikolascastro87@gmail.com>
2022-07-23 12:00:13 -0300

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,10 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/medtools/'
+
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
